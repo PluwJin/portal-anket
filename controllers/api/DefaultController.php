22289a98e48c9fd98b3c9d64dd0041f9ff6ed73e
PluwJin <ozerhan9@gmail.com>
2018-12-07 12:49:35 +0300

    second commit

---
+++ controllers/api/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+namespace kouosl\anket\controllers\api;
+
+
+/**
+ * Default controller for the `anket` module
+ */
+class DefaultController extends \kouosl\base\controllers\api\BaseController
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('_index');
+    }
+}
---
+++ controllers/frontend/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+namespace kouosl\anket\controllers\frontend;
+
+
+/**
+ * Default controller for the `anket` module
+ */
+class DefaultController extends \kouosl\base\controllers\frontend\BaseController
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('_index');
+    }
+}
