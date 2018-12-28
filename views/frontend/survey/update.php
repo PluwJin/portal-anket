4fc2dba181976c60ae2f6c2f636016e934d35462
PluwJin <ozerhan9@gmail.com>
2018-12-28 23:55:15 +0300

    soru silme ve ekleme

--- views/frontend/survey/update.php
+++ views/frontend/survey/update.php
@@ -1,6 +1,9 @@
 <?php
 
 use yii\helpers\Html;
+use kouosl\anket\models\Questions;
+use yii\data\ActiveDataProvider;
+use yii\grid\GridView;
 
 /* @var $this yii\web\View */
 /* @var $model kouosl\anket\models\Survey */
@@ -9,6 +12,7 @@ $this->title = 'Anket Güncelleme: ' . $model->name;
 $this->params['breadcrumbs'][] = ['label' => 'Surveys', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
+
 ?>
 
 <style>
@@ -17,31 +21,53 @@ $this->params['breadcrumbs'][] = 'Update';
     
     border-radius:25px;
 }
-.sec{
-    margin:50px;
-    margin-left:350px;
-    
-}
 h1{
     font-family:Century Gothic;
     margin-bottom:50px;
     color:red;
+    text-align:center;
 }
-p{
-    display:inline;
-   
+.Grid{
+    margin :80px;
 }
+h3{
+    margin-left:80px;
+}
+
 
 </style>
 
 
 <div class="row">
+<h1><?= Html::encode($this->title) ?></h1>
 <div class="sec" >
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    
      <?php $_SESSION['SurveyId']=$model->id; ?>
-    <p><a class="btn btn-lg btn-success" href="/anket/create">Soru Çıkar</a></p>
-    <?= Html::a('Soru Ekle','/anket/survey',['class'=>'btn btn-primary']) ?>
+     <?php $_SESSION['Qnumber']=1; ?>
+
+
+     <?php $dataProvider = new ActiveDataProvider([
+    'query' => Questions::find()->where(['s_id'=>$model->id]),
+    'pagination'=>['pageSize'=>20],
+]);  ?>
+<h3> Anket Soruları Silmek veya Eklemek için Butonları Kullanın:</h3>
+<div class="Grid">
+     <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+
+            'id',
+            's_id',
+            'name',
+            'type',
+            'required',
+            'option_number',
+            ['class' => 'yii\grid\ActionColumn', 'template' => '{delete}']
+        
+    ]]); ?> 
+    </div>
+         <?= Html::a('Soru Ekle','/anket/create/step2',['class'=>'btn btn-lg btn-success']) ?>
 
     <?php //$this->render('_form', [ 'model' => $model ]) ?>
     </div>
